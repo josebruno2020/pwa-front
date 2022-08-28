c75d3a9e5585683aaf6eed503114c4d5a86c5ebb
José Bruno <josebrunocampanholi@gmail.com>
2022-08-28 17:37:44 -0300

    :pizza: continuacao formulario

--- src/components/notifications/AutoPersonal.vue
+++ src/components/notifications/AutoPersonal.vue
@@ -275,22 +275,31 @@
           </el-select>
         </el-form-item>
 
+      </div>
+      <div class="form-flex">
+        <el-form-item class="form-choose" v-if="model._39" label="39- Qual tipo de deficiência /transtorno?">
+          <div v-for="(v, index) in value39" :key="index" class="form-select-item">
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._39[v.value] }}</strong>
+            </div>
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._39, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._39, 2, v.value)">Não</el-button>
+              <el-button plain type="warning" size="mini" @click="setInformationInArray(model._39, 8, v.value)">Não se aplica</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._39, 9, v.value)">Ignorado</el-button>
+            </div>
 
-        <el-form-item label="39- Qual tipo de deficiência /transtorno?">
-          <el-select v-model="model._39" clearable placeholder="selecione..." filterable multiple>
-            <el-option
-                v-for="(v, index) in value39"
-                :key="index"
-                :label="v.label"
-                :value="v.value">
-            </el-option>
-          </el-select>
+
+          </div>
         </el-form-item>
 
-        <el-form-item   v-if="model._39.includes(7)" label="39- Outras">
+        <el-form-item  label="39- Outras">
           <el-input v-model="model._3905"></el-input>
         </el-form-item>
       </div>
+
 
       <p class="subtitle">Dados da Ocorrência</p>
 
@@ -305,6 +314,286 @@
             </el-option>
           </el-select>
         </el-form-item>
+
+        <el-form-item  label="41- Município de ocorrência">
+          <el-input v-model="model._41"></el-input>
+        </el-form-item>
+
+        <el-form-item label="41- Código IBGE">
+          <el-input v-model="model._4105" maxlength="6" @input="onlyNumbers($event)"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="42- Distrito">
+          <el-input v-model="model._42"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="43- Bairro">
+          <el-input v-model="model._43"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="44- Logradouro (rua, avenida,...)">
+          <el-input v-model="model._44"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="44- Código">
+          <el-input v-model="model._4405" maxlength="6"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="45- Número">
+          <el-input v-model="model._45"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="46- Complemento (apto., casa, ...)">
+          <el-input v-model="model._46"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="47- Geo campo 3">
+          <el-input v-model="model._47"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="48- Geo campo 4">
+          <el-input v-model="model._48"></el-input>
+        </el-form-item>
+
+        <el-form-item  label="49- Ponto de Referência">
+          <el-input v-model="model._49"></el-input>
+        </el-form-item>
+
+        <el-form-item label="50- Zona">
+          <el-select v-model="model._50" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value31"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item  label="51- Hora da ocorrência">
+          <el-input v-model="model._51" type="time"></el-input>
+        </el-form-item>
+
+        <el-form-item label="52- Local de ocorrência">
+          <el-select v-model="model._52" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value52"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item v-if="model._52 && model._52 === '09'"  label="52- Outro">
+          <el-input v-model="model._5205"></el-input>
+        </el-form-item>
+
+        <el-form-item label="53- Ocorreu outras vezes?">
+          <el-select v-model="model._53" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value53"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="54- A lesão foi autoprovocada?">
+          <el-select v-model="model._54" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value54"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </div>
+
+      <p class="subtitle">Violência</p>
+      <div class="form-flex">
+        <el-form-item label="55- Essa violência foi motivada por">
+          <el-select v-model="model._55" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value55"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item v-if="model._55 && model._55 === '09'"  label="55- Outro">
+          <el-input v-model="model._5505"></el-input>
+        </el-form-item>
+
+        <el-form-item class="form-choose" v-if="model._56" label="56- Tipo de violência">
+          <div v-for="(v, index) in value56" :key="index" class="form-select-item">
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._56, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._56, 2, v.value)">Não</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._56, 9, v.value)">Ignorado</el-button>
+            </div>
+
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._56[v.value] }}</strong>
+            </div>
+
+          </div>
+        </el-form-item>
+
+        <el-form-item  label="56- Outras">
+          <el-input v-model="model._5605"></el-input>
+        </el-form-item>
+
+        <el-form-item class="form-choose" v-if="model._57" label="57- Meio de agressão">
+          <div v-for="(v, index) in value57" :key="index" class="form-select-item">
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._57, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._57, 2, v.value)">Não</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._57, 9, v.value)">Ignorado</el-button>
+            </div>
+
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._57[v.value] }}</strong>
+            </div>
+
+          </div>
+        </el-form-item>
+
+        <el-form-item  label="57- Outras">
+          <el-input v-model="model._5705"></el-input>
+        </el-form-item>
+      </div>
+
+      <p class="subtitle">Violência Sexual</p>
+      <div class="form-flex">
+        <el-form-item class="form-choose" v-if="model._58" label="58- Se ocorreu violência sexual, qual o tipo?">
+          <div v-for="(v, index) in value58" :key="index" class="form-select-item">
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._58, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._58, 2, v.value)">Não</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._58, 9, v.value)">Ignorado</el-button>
+            </div>
+
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._58[v.value] }}</strong>
+            </div>
+
+          </div>
+        </el-form-item>
+
+        <el-form-item  label="58- Outro">
+          <el-input v-model="model._5805"></el-input>
+        </el-form-item>
+
+        <el-form-item class="form-choose" v-if="model._59" label="59- Procedimento realizado">
+          <div v-for="(v, index) in value59" :key="index" class="form-select-item">
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._59, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._59, 2, v.value)">Não</el-button>
+              <el-button plain type="warning" size="mini" @click="setInformationInArray(model._59, 8, v.value)">Não se aplica</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._59, 9, v.value)">Ignorado</el-button>
+            </div>
+
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._59[v.value] }}</strong>
+            </div>
+
+          </div>
+        </el-form-item>
+
+      </div>
+      <p class="subtitle">Dados do provável autor da violência</p>
+      <div class="form-flex">
+
+        <el-form-item label="60- Número de envolvidos">
+          <el-select v-model="model._60" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value60"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+
+        <el-form-item class="form-choose" v-if="model._61" label="61- Vínculo/grau de parentesco com a pessoa atendida">
+          <div v-for="(v, index) in value61" :key="index" class="form-select-item">
+            <div>
+              <el-button plain type="success" size="mini" @click="setInformationInArray(model._61, 1, v.value)">Sim</el-button>
+              <el-button plain type="danger" size="mini" @click="setInformationInArray(model._61, 2, v.value)">Não</el-button>
+              <el-button plain type="info" size="mini" @click="setInformationInArray(model._61, 9, v.value)">Ignorado</el-button>
+            </div>
+
+            <div>
+              {{ v.label }}
+              -
+              <strong>{{ model._61[v.value] }}</strong>
+            </div>
+
+          </div>
+        </el-form-item>
+
+        <el-form-item label="62- Sexo do provável autor da violência">
+          <el-select v-model="model._62" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value62"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="63- Suspeita de uso de álcool">
+          <el-select v-model="model._63" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value63"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="64- Ciclo de vida do provável autor da violência:">
+          <el-select v-model="model._64" clearable placeholder="selecione..." filterable>
+            <el-option
+                v-for="(v, index) in value64"
+                :key="index"
+                :label="v.label"
+                :value="v.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+      </div>
+
+      <p class="subtitle">Encaminhamento</p>
+      <div class="form-flex">
+
+      </div>
+
+      <p class="subtitle">Dados finais</p>
+      <div class="form-flex">
+
+      </div>
+
+      <h5>Informações complementares e observações</h5>
+      <div class="form-flex">
+
       </div>
 
       <div class="is-flex is-justify-end">
@@ -319,16 +608,14 @@
 import {Component, Vue} from "vue-property-decorator";
 import {PatientModel} from "@/models/PatientModel";
 import {states} from "@/helpers/form/states";
-import {_06, _13, _14, _15, _31, _35, _36, _37, _38, _39} from "@/helpers/notifications/arraysToChoose";
+import {_06, _13, _14, _15, _31, _35, _36, _37, _38, _39, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64} from "@/helpers/notifications/arraysToChoose";
 
 @Component({
   name: 'AutoPersonal'
 })
 export default class AutoPersonal extends Vue {
   patient: PatientModel = new PatientModel()
-  model = {
-    _39: []
-  }
+  model = {}
 
   now = new Date()
   loading = false
@@ -343,11 +630,38 @@ export default class AutoPersonal extends Vue {
   value37 = _37
   value38 = _38
   value39 = _39
+  value52 = _52
+  value53 = _53
+  value54 = _54
+  value55 = _55
+  value56 = _56
+  value57 = _57
+  value58 = _58
+  value59 = _59
+  value60 = _60
+  value61 = _61
+  value62 = _62
+  value63 = _63
+  value64 = _64
+
+  selectedValue39: number[] = []
+
+  created() {
+    this.model = {
+      _39: [],
+      _56: [],
+      _57: [],
+      _58: [],
+      _59: [],
+      _61: [],
+      _65: []
+    }
+  }
 
   setInformation(patient: PatientModel) {
     this.patient = patient
-    console.log(this.patient)
     this.model = {
+      ...this.model,
       _10: this.patient.name,
       _11: this.patient.birthdate,
       _18: this.patient.name_mother,
@@ -357,8 +671,9 @@ export default class AutoPersonal extends Vue {
       _23: this.patient.street,
       _24: this.patient.number,
       _30: this.patient.phone_number,
-      _32: 'Brasil'
+      _32: 'Brasil',
     }
+    this.selectedValue39 = []
   }
 
   private onlyNumbers(value: string) {
@@ -366,6 +681,17 @@ export default class AutoPersonal extends Vue {
   }
 
   submitForm() {
+    console.log(Object.entries(this.model))
+    //confirmar para salvar;
+  }
+
+
+  private setInformationInArray(model: any, value: number, valueInArray: number): void {
+    Vue.set(model, valueInArray, value)
+  }
+
+  async handle() {
+    alert('handle')
     console.log(Object.entries(this.model))
   }
 }
@@ -388,4 +714,10 @@ export default class AutoPersonal extends Vue {
 
 .el-select, .el-input
   width: 250px
+
+.form-choose
+  width: 330px
+
+//.form-select-item
+//  display: flex
 </style>
--- src/helpers/notifications/arraysToChoose.ts
+++ src/helpers/notifications/arraysToChoose.ts
@@ -243,5 +243,419 @@ export const _39 = [
         label: 'Outras',
         value: 7
     },
+]
 
+export const _52 = [
+    {
+        label: '01 - Residência',
+        value: '01'
+    },
+    {
+        label: '02 - Habitação coletiva',
+        value: '02'
+    },
+    {
+        label: '03 - Escola',
+        value: '03'
+    },
+    {
+        label: '04 - Local de prática esportiva',
+        value: '04'
+    },
+    {
+        label: '05 - Bar ou similar',
+        value: '05'
+    },
+    {
+        label: '06 - Via pública',
+        value: '06'
+    },
+    {
+        label: '07 - Comércio/serviços',
+        value: '07'
+    },
+    {
+        label: '08 - Indústrias/construção',
+        value: '08'
+    },
+    {
+        label: '09 - Outro',
+        value: '09'
+    },
+    {
+        label: '99 - Ignorado',
+        value: '99'
+    },
+]
+
+export const _53 = [
+    {
+        label: '1- Sim',
+        value: 1
+    },
+    {
+        label: '2- Não',
+        value: 2
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
+]
+
+export const _54 = [
+    {
+        label: '1- Sim',
+        value: 1
+    },
+    {
+        label: '2- Não',
+        value: 2
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
+]
+
+
+export const _55 = [
+    {
+        label: '01- Sexismo',
+        value: '01'
+    },
+    {
+        label: '02- Homofobia/Lesbofobia/Bifobia/Transfobia',
+        value: '02'
+    },
+    {
+        label: '03- Racismo',
+        value: '03'
+    },
+    {
+        label: '04- Intolerância religiosa',
+        value: '04'
+    },
+    {
+        label: '05- Xenofobia',
+        value: '05'
+    },
+    {
+        label: '06- Conflito geracional',
+        value: '06'
+    },
+    {
+        label: '07- Situação de rua',
+        value: '07'
+    },
+    {
+        label: '08- Deficiência',
+        value: '08'
+    },
+    {
+        label: '09- Outro',
+        value: '09'
+    },
+    {
+        label: '88- Não se aplica',
+        value: '99'
+    },
+    {
+        label: '99 - Ignorado',
+        value: '99'
+    },
+]
+
+
+export const _56 = [
+    {
+        label: 'Física',
+        value: 1
+    },
+    {
+        label: 'Psicológica/Moral',
+        value: 2
+    },
+    {
+        label: 'Tortura',
+        value: 3
+    },
+    {
+        label: 'Sexual',
+        value: 4
+    },
+    {
+        label: 'Tráfico de seres humanos',
+        value: 5
+    },
+    {
+        label: 'Financeira/Econômica',
+        value: 6
+    },
+    {
+        label: 'Negligência/Abandono',
+        value: 7
+    },
+    {
+        label: 'Trabalho infantil',
+        value: 8
+    },
+    {
+        label: 'Intervenção legal',
+        value: 9
+    },
+    {
+        label: 'Outros',
+        value: 10
+    },
+]
+
+export const _57 = [
+    {
+        label: 'Força corporal/espancamento',
+        value: 1
+    },
+    {
+        label: 'Enforcamento',
+        value: 2
+    },
+    {
+        label: 'Obj. contundente',
+        value: 3
+    },
+    {
+        label: 'Obj. pérfurocortante',
+        value: 4
+    },
+    {
+        label: 'Substância/Obj. quente',
+        value: 5
+    },
+    {
+        label: 'Envenenamento, Intoxicação',
+        value: 6
+    },
+    {
+        label: 'Arma de fogo',
+        value: 7
+    },
+    {
+        label: 'Ameaça',
+        value: 8
+    },
+    {
+        label: 'Outro',
+        value: 9
+    }
+]
+
+export const _58 = [
+    {
+        label: 'Assédio sexual',
+        value: 1
+    },
+    {
+        label: 'Estupro',
+        value: 2
+    },
+    {
+        label: 'Pornografia infantil',
+        value: 3
+    },
+    {
+        label: 'Exploração sexual ',
+        value: 4
+    },
+    {
+        label: 'Outros',
+        value: 5
+    },
+]
+
+export const _59 = [
+    {
+        label: 'Profilaxia DST',
+        value: 1
+    },
+    {
+        label: 'Profilaxia HIV',
+        value: 2
+    },
+    {
+        label: 'Profilaxia Hepatite B',
+        value: 3
+    },
+    {
+        label: 'Coleta de sangue',
+        value: 4
+    },
+    {
+        label: 'Coleta de sêmen ',
+        value: 5
+    },
+    {
+        label: 'Coleta de secreção vagina',
+        value: 6
+    },
+    {
+        label: 'Contracepção de emergência',
+        value: 7
+    },
+    {
+        label: 'Aborto previsto em le',
+        value: 8
+    }
+]
+
+export const _60 = [
+    {
+        label: '1- Sim',
+        value: 1
+    },
+    {
+        label: '2- Não',
+        value: 2
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
+]
+
+
+export const _61 = [
+    {
+        label: 'Pai',
+        value: 1
+    },
+    {
+        label: 'Mãe',
+        value: 2
+    },
+    {
+        label: 'Padrasto',
+        value: 3
+    },
+    {
+        label: 'Madrasta',
+        value: 4
+    },
+    {
+        label: 'Cônjuge',
+        value: 5
+    },
+    {
+        label: 'Ex-Cônjuge',
+        value: 6
+    },
+    {
+        label: 'Namorado(a)',
+        value: 7
+    },
+    {
+        label: 'Ex-Namorado(a)',
+        value: 8
+    },
+    {
+        label: 'Filho(a)',
+        value: 9
+    },
+    {
+        label: 'Irmão(ã)',
+        value: 10
+    },
+    {
+        label: 'Amigos/conhecidos',
+        value: 11
+    },
+    {
+        label: 'Desconhecido(a)',
+        value: 12
+    },
+    {
+        label: 'Cuidador(a)',
+        value: 13
+    },
+    {
+        label: 'Patrão/chefe',
+        value: 14
+    },
+    {
+        label: 'Pessoa com relação institucional',
+        value: 15
+    },
+    {
+        label: 'Policial/agente da lei',
+        value: 16
+    },
+    {
+        label: 'Própria pessoa',
+        value: 17
+    },
+    {
+        label: 'Outros',
+        value: 18
+    },
+]
+
+export const _62 = [
+    {
+        label: '1 - Masculino',
+        value: 1
+    },
+    {
+        label: '2 - Feminino',
+        value: 2
+    },
+    {
+        label: '3 - Ambos os sexos',
+        value: 3
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
+]
+
+
+export const _63 = [
+    {
+        label: '1- Sim',
+        value: 1
+    },
+    {
+        label: '2- Não',
+        value: 2
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
+]
+
+export const _64 = [
+    {
+        label: '1- Criança (0 a 9 anos)',
+        value: 1
+    },
+    {
+        label: '2- Adolescente (10 a 19 anos)',
+        value: 2
+    },
+    {
+        label: '3- Jovem (20 a 24 anos)',
+        value: 3
+    },
+    {
+        label: '4- Pessoa adulta (25 a 59 anos)',
+        value: 4
+    },
+    {
+        label: '5- Pessoa idosa (60 anos ou mais)',
+        value: 5
+    },
+    {
+        label: '9- Ignorado',
+        value: 9
+    }
 ]
